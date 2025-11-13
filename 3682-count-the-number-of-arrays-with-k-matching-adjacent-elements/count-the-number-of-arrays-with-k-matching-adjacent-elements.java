692e3ec740521176bc337f0d2156ee9ae7aaa5a3
Muzamil Ahmed Bhayo <f23ari40@aror.edu.pk>
2025-11-13 20:33:10 +0500

    Time: 993 ms (5.88%) | Memory: 42.7 MB (73.53%) - LeetSync

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.java
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.java
@@ -1,34 +1,37 @@
 class Solution {
-    static final int MOD = 1_000_000_007;
-    private long pow(long base, long exp) {
+    private static final int MOD = 1_000_000_007;
+
+    public int countGoodArrays(int n, int m, int k) {
+        if (k >= n) return 0;
+
+        long comb = nCrMod(n - 1, k, MOD);
+        long pow = modPow(m - 1, n - 1 - k, MOD);
+        long res = comb * m % MOD * pow % MOD;
+        return (int) res;
+    }
+
+    private long modPow(long base, long exp, long mod) {
         long res = 1;
+        base %= mod;
         while (exp > 0) {
-            if ((exp & 1) == 1) res = (res * base) % MOD;
-            base = (base * base) % MOD;
+            if ((exp & 1) == 1) res = res * base % mod;
+            base = base * base % mod;
             exp >>= 1;
         }
         return res;
     }
-    private long modInverse(long x) {
-        return pow(x, MOD - 2);
+
+    private long modInverse(long a, long mod) {
+        return modPow(a, mod - 2, mod); // Fermat's little theorem
     }
-    private long nCr(int n, int r, long[] fact, long[] invFact) {
+
+    private long nCrMod(int n, int r, int mod) {
         if (r < 0 || r > n) return 0;
-        return (((fact[n] * invFact[r]) % MOD) * invFact[n - r]) % MOD;
-    }
-
-    public int countGoodArrays(int n, int m, int k) {
-       
-        long[] fact = new long[n + 1];
-        long[] invFact = new long[n + 1];
-        fact[0] = 1;
-        for (int i = 1; i <= n; i++) fact[i] = (fact[i - 1] * i) % MOD;
-        invFact[n] = modInverse(fact[n]);
-        for (int i = n - 1; i >= 0; i--) invFact[i] = (invFact[i + 1] * (i + 1)) % MOD;
-        long res = m;
-        res = (res * nCr(n - 1, k, fact, invFact)) % MOD;
-        res = (res * pow(m - 1, n - 1 - k)) % MOD;
-
-        return (int) res;
+        long res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (n - i + 1) % mod;
+            res = res * modInverse(i, mod) % mod;
+        }
+        return res;
     }
 }
