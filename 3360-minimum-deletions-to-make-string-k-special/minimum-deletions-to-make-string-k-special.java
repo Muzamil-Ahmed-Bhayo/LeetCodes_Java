5e62d09dff28e08f6bc5ca7d37351856e3238ff8
Muzamil Ahmed Bhayo <f23ari40@aror.edu.pk>
2025-11-14 15:01:40 +0500

    Time: 24 ms (30.91%) | Memory: 47.6 MB (27.64%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
@@ -1,42 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int minimumDeletions(String word, int k) {
         int[] freq = new int[26];
-        for (char c : word.toCharArray()) {
-            freq[c - 'a']++;
-        }
+        for (char c : word.toCharArray()) freq[c - 'a']++;
 
-        List<Integer> list = new ArrayList<>();
-        for (int f : freq) {
-            if (f > 0) list.add(f);
-        }
+        int max = 0;
+        for (int f : freq) if (f > max) max = f;
 
-        Collections.sort(list);
-        int n = list.size();
         int ans = Integer.MAX_VALUE;
-        int[] prefix = new int[n + 1];
-        for (int i = 0; i < n; i++) prefix[i + 1] = prefix[i] + list.get(i);
 
-        for (int i = 0; i < n; i++) {
-            int targetMin = list.get(i);
-            int maxAllowed = targetMin + k;
-            int j = upperBound(list, maxAllowed);
-            int delLeft = prefix[i];
-            int delRight = (prefix[n] - prefix[j]) - (maxAllowed * (n - j));
-            ans = Math.min(ans, delLeft + delRight);
+        for (int target = 0; target <= max; target++) {
+            int del = 0;
+            for (int f : freq) {
+                if (f < target) del += f;
+                else if (f > target + k) del += f - (target + k);
+            }
+            ans = Math.min(ans, del);
         }
 
-        return ans == Integer.MAX_VALUE ? 0 : ans;
-    }
-
-    private int upperBound(List<Integer> list, int val) {
-        int l = 0, r = list.size();
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (list.get(mid) <= val) l = mid + 1;
-            else r = mid;
-        }
-        return l;
+        return ans;
     }
 }
